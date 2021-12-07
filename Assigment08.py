5c3c2b07f912fce952dca3f32b407715bc001843
Ryan Fear <rafear91@gmail.com>
2021-12-06 20:18:26 -0800

    Initial commit
    Initial publish of files

---
+++ Assigment08.py
@@ -0,0 +1,234 @@
+# ------------------------------------------------------------------------ #
+# Title: Assignment 08
+# Description: Working with classes
+
+# ChangeLog (Who,When,What):
+# RRoot,1.1.2030,Created started script
+# RRoot,1.1.2030,Added pseudo-code to start assignment 8
+# RFear,12.5.2021,Modified code to complete assignment 8
+# ------------------------------------------------------------------------ #
+
+# Data -------------------------------------------------------------------- #
+file_name = 'products.txt'
+lstProductObjects = []
+
+
+class Product:
+    """Stores data about a product:
+
+    properties:
+        product_name: (string) with the products name
+        product_price: (float) with the products standard price
+    methods:
+        __str__: returns a string
+        __float__: returns a float
+    changelog: (Who,When,What)
+        RRoot,1.1.2030,Created Class
+        RFear,12.5.2021,Modified code to complete assignment 8
+    """
+
+    # --Constructors--
+    def __init__(self, product_name, product_price):
+        # --Attributes--
+        self.product_name = product_name
+        self.product_price = product_price
+
+    ## --Properties--
+    @property
+    def product_name(self):
+        return str(self.__product_name).title()
+
+    @product_name.setter
+    def product_name(self, name):
+        if str(name).isnumeric() == False:
+            self.__product_name = name
+        else:
+            raise Exception("Names cannot be numbers.")
+
+    @property
+    def product_price(self):
+        return '{:.2f}'.format(self.__product_price)
+
+    @product_price.setter
+    def product_price(self, value):
+        if type(value) == float:
+            self.__product_price = value
+        else:
+            raise Exception("Prices cannot be strings.")
+
+    ## --Methods--
+    def __str__(self):
+        return self.product_name
+
+    def __float__(self):
+        return self.product_price
+
+
+# Processing  ------------------------------------------------------------- #
+class FileProcessor:
+    """Processes data to and from a file and a list of product objects:
+
+    methods:
+        read_data_from_file(file_name, list_of_product_objects): -> (a list of product objects)
+
+        save_data_to_file(file_name, list_of_product_objects):
+
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+        RFear,12.5.2021,Modified code to complete assignment 8
+    """
+
+    @staticmethod
+    def read_data(fname, list_of_obj):
+        try:
+            f = open(fname, 'r')
+            for line in f:
+                prod, price = line.split(',')
+                prod, price = str(prod.strip()), float(price.strip())
+                row = Product(prod, price)
+                list_of_obj.append(row)
+        except FileNotFoundError:
+            print(f"There is no file with the name '{fname}'.  One will be created.")
+            f = open(fname, "w")
+            f.close()
+            print(f"\nThe file '{file_name}' was created.")
+        else:
+            print(f"Successfully read data from the file '{fname}'.")
+            f.close()
+
+    @staticmethod
+    def save_data(fname, list_of_obj):
+        """ Writes data from a list to a file
+
+        :param fname: (string) name of file to save data to:
+        :param list_of_obj: (list) list of data to write to file:
+        """
+        f = open(fname, 'w')
+        for row in list_of_obj:
+            f.write(row.product_name + "," + row.product_price + "\n")
+        f.close()
+
+
+# Presentation (Input/Output)  -------------------------------------------- #
+class IO():
+    """Description goes here:
+
+    methods:
+        TO BE UPDATED
+
+    changelog: (When,Who,What)
+        RRoot,1.1.2030,Created Class
+        RFear,12.5.2021,Modified code to complete assignment 8
+    """
+
+    @staticmethod
+    def print_menu():
+        """  Display a menu of choices to the user
+
+        :return: nothing
+        """
+        print('''
+        Menu of Options
+        1) Show Current Data
+        2) Add Product and Price
+        3) Save Data to File and Exit
+        ''')
+
+    @staticmethod
+    def input_menu_choice():
+        """ Gets the menu choice from a user
+
+        :return: string
+        """
+        choice = str(input("Which option would you like to perform? [1 to 3] - ")).strip()
+        print()  # Add an extra line for looks
+        return choice
+
+    @staticmethod
+    def input_yes_no_choice(message):
+        """ Gets a yes or no choice from the user
+
+        :return: string
+        """
+        return str(input(message)).strip().lower()
+
+    @staticmethod
+    def print_current_data(list_of_obj):
+        """ Shows the current Tasks in the list of dictionaries rows
+
+        :param list_of_obj: (list) of data you want to display
+        :return: nothing
+        """
+        if list_of_obj == []:
+            print("There is no data in the file.")
+        else:
+            print("The current products and prices are: ")
+            print("The output format is:")
+            print("Product" + " | " + "Price")
+            for row in list_of_obj:
+                print(row.product_name, " | ", row.product_price)
+
+    # TODO: Add code to get product data from user
+    @staticmethod
+    def add_data(list_of_obj):
+        product = ""
+        price = ""
+        ans = None
+
+        while ans != "y":
+            product = input("Enter a new product name: ")
+            ans = input(f"\nFor the product you entered: {product}\n"
+                        f"Are these results correct? [y/n] ")
+
+        while True:
+            try:
+                price = float(input("\nEnter the price for the new product: "))
+            except ValueError as e:
+                print("Please enter a number for the price.\n")
+            if type(price) == float:
+                break
+
+        row = Product(product, price)
+        list_of_obj.append(row)
+
+
+# Presentation (Input/Output)  -------------------------------------------- #
+
+# Main Body of Script  ---------------------------------------------------- #
+# Load data from file into a list of product objects when script starts
+# Show user a menu of options
+# Get user's menu option choice
+# Option 1: Show user current data in the list of product objects
+# Option 2: Let user add data to the list of product objects
+# Option 3: Let user save current data to file and exit program
+# Main Body of Script  ---------------------------------------------------- #
+
+# Make file object, open, and read data
+FileProcessor.read_data(file_name, lstProductObjects)
+
+while True:
+    IO.print_menu()  # shows menu
+    choice = IO.input_menu_choice()
+
+    if choice == '1':  # show current data
+        IO.print_current_data(lstProductObjects)
+
+    elif choice == '2':  # add product and price
+        IO.add_data(lstProductObjects)
+
+    elif choice == '3':  # save and exit
+        valid = ['y', 'n']
+        while True:
+            yn_choice = IO.input_yes_no_choice(f"Save data and to '{file_name}' and exit? [y/n]: ")
+            if yn_choice in valid:
+                if yn_choice == "n":
+                    print("Save cancelled. Exiting program.")
+                else:
+                    FileProcessor.save_data(file_name, lstProductObjects)
+                    print("Data saved to file. Exiting program.")
+                break
+        break
+
+    else:
+        print("Error: Cannot process request."
+              "\nPlease enter a valid option.")
